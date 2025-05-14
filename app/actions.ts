fe314f1143bfadfa0a435b867b942acf0d75ee0a
Andrew Qu <qual1337@gmail.com>
2025-05-14 10:31:37 -0700

    app/actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,9 @@
-'use server'
+"use server";
 
 export async function getDelayedData(delay: number) {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
-  const res = await fetch(`${baseUrl}/api/delay?delay=${delay}`, {
-    cache: 'no-store'
-  })
-  return res.json()
-} 
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const res = await fetch(`${baseUrl}/api/${delay}/delay`, {
+    cache: "no-store",
+  });
+  return res.json();
+}
