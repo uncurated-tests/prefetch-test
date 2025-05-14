75e2fd13145f41812f28c0bea6b4b6847fed4b68
Andrew Qu <qual1337@gmail.com>
2025-05-14 10:28:02 -0700

    app/(routes)/[delay]/layout.tsx

--- app/(routes)/[delay]/layout.tsx
+++ app/(routes)/[delay]/layout.tsx
@@ -1,10 +1,5 @@
 export function generateStaticParams() {
-  return [
-    { delay: "0" },
-    { delay: "1000" },
-    { delay: "2000" },
-    { delay: "100000" },
-  ];
+  return [{ delay: "0" }, { delay: "2000" }, { delay: "10000" }];
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
